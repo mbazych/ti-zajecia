2dcf5a9566892a98af355c40db4f604f4347459e
Mateusz Czarczyński <mczarczynski@edu.cdv.pl>
2020-05-19 12:22:16 +0200

    asd

--- scripts/add_user.php
+++ scripts/add_user.php
@@ -1,6 +1,12 @@
 <?php
     session_start();
-    if(!empty($_POST['name']) &&  !empty($_POST['surname']) && !empty($_POST['email1']) && !empty($_POST['email2']) && !empty($_POST['pass1']) && !empty($_POST['pass2']) && !empty($_POST['birthday'])){
+    if(!empty($_POST['name'])
+    && !empty($_POST['surname'])
+    && !empty($_POST['email1'])
+    && !empty($_POST['email2']) 
+    && !empty($_POST['pass1']) 
+    && !empty($_POST['pass2']) 
+    && !empty($_POST['birthday'])){
 
         if(!isset($_POST['terms'])){
             $_SESSION['error'] = "Zaznacz pole z regulaminem";

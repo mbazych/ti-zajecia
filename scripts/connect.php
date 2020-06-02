edb2c1330c507ec233aa9aa93b9bb25c00520d38
mbazych <44398475+mbazych@users.noreply.github.com>
2020-06-02 19:44:44 +0200

    Revert "Merge branch 'master' of https://github.com/mbazych/ti-zajecia"
    This reverts commit b7c2850056ac84bf4b48a0453e631cd32a3e37d3, reversing
    changes made to ff4fc25d93c12e7d4add7d11d4e107222ce06465.

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,4 +1,5 @@
 <?php  
-    $conn = new mysqli('localhost:3306','cdv','cdv','cdv_ti');
+    // $conn = new mysqli('localhost','root','','cdv_ti');
+    $conn = new mysqli('localhost','cdv','cdv','cdv_ti');
     $conn->set_charset('utf8');
 ?>

40e45e138f4ab23372690b406d38fd186fbd548f
Michał Bazych <mbazych@centuria.pl>
2020-06-06 15:03:53 +0200

    edit docker-compose.yml

--- src/login.php
+++ src/login.php
@@ -1,3 +1,6 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -42,37 +45,40 @@
                     <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                     <div class="divider-custom-line"></div>
                 </div>
+                <?php
+                if(isset($_SESSION['error'])){
+                  echo '<div class="col-md1">
+                          <div class="card">
+                            <div class="text-center card-header card-text text-secondary">
+                            '.$_SESSION['error'].'
+                            </div>
+                          </div>
+                        </div>
+                        ';
+                        unset($_SESSION['error']);
+                }
+                ?>
+                <form action="./scripts/login_user.php" method="post">
+
+                  <div class="form-group form-group-lg">
+                    <label for="inputEmail">Email address</label>
+                    <input name="email" type="email" class="form-control-lg form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+                  </div>               
+                  <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input name="pass" type="password" class="form-control-lg form-control" id="inputPassword" aria-describedby="passwordHelp" placeholder="Enter password">
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="rememberCheck">
+                    <label class="form-check-label" for="rememberCheck">Remember me</label>
+                  </div>
+                  <button type="submit" class="btn btn-dark  btn-lg">Login</button>
+
+
+                  </div>
+                </form>
             </div>
         </header>
-        <!-- Portfolio Section-->
-        <section class="page-section portfolio" id="portfolio">
-          
-        </section>
-        <!-- About Section-->
-        
-        <!-- Contact Section-->
-        <section class="page-section" id="contact">
-            <div class="container">
-                <!-- Contact Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact us!</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Contact Section Form-->
-                <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
-                        <p>Phone: 123123123</p>
-                        <p>Nadbrzeżna 21 </p>
-                        <p>72-320 Trzebiatów</p>
-                        <p>NIP: 123123123123</p>
-                    </div>
-                </div>
-            </div>
-        </section>
         <!-- Footer-->
         <footer class="footer text-center">
             <div class="container">

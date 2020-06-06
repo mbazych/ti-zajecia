3587ba92cb63d44ddf3745ffea3eb346c9b75720
Michał Bazych <mbazych@centuria.pl>
2020-06-06 15:32:27 +0200

    test

--- src/scripts/connect.php
+++ src/scripts/connect.php
@@ -1,5 +1,5 @@
 <?php  
     // $conn = new mysqli('localhost','root','','cdv_ti');
-    $conn = new mysqli('localhost','root','docker','eventano');
+    $conn = new mysqli('mysql:host=database;dbname=eventano;charset=utf8mb4','root','docker','eventano');
     $conn->set_charset('utf8');
 ?>

1dc05d2bd4a7c432cd7d427707d496ef5f250527
root <root@DESKTOP-B3KDVC4.localdomain>
2020-06-06 13:29:31 +0200

    add login

--- src/register.php
+++ src/register.php
@@ -1,39 +1,124 @@
 <?php
   session_start();
 ?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>EVENTANO</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="./static/assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="static/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><?php echo 'EVENTANO'?></a><button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Events</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="./login.php">Login</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="./register.php">Register</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <h1 class="masthead-heading text-uppercase mb-0">EVENTANO</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <?php
+                if(isset($_SESSION['error'])){
+                  echo '<div class="col-md1">
+                          <div class="card card-outline card-danger">
+                            <div class="card-header">
+                            '.$_SESSION['error'].'
+                            </div>
+                          </div>
+                          <!-- /.card -->
+                        </div>
+                        ';
+                        unset($_SESSION['error']);
+                }
+                ?>
+                <form action="./scripts/add_user.php" method="post">
 
-    <title>Register</title>
+                  <div class="form-group form-group-lg">
+                    <label for="inputEmail">Email address</label>
+                    <input name="email" type="email" class="form-control-lg form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control-lg form-control" id="inputPassword" aria-describedby="passwordHelp" placeholder="Enter safe password">
+                  </div>
+                  <div class="form-group">
+                    <label for="inputDate">Birthday date</label>
+                    <input type="date" class="form-control-lg form-control" id="inputDate" aria-describedby="dateHelp" placeholder="Enter birthday date">
+                  </div>
+                  <button type="submit" class="btn btn-dark  btn-lg">Register</button>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
 
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+                  </div>
+                </form>
+            </div>
+        </header>
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
 
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
-  </head>
-
-  <body class="text-center">
-    <form class="form-signin">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please register</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      <label for="inputDate" class="sr-only">Birthday date</label>
-      <input type="date" id="inputDate" class="form-control" placeholder="Date" required>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-      <p class="mt-5 mb-3 text-muted">mbazych.pl &copy; 2020</p>
-    </form>
-  </body>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-6 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-6">
+                        <h4 class="text-uppercase mb-4">About EVENTANO</h4>
+                        <p class="lead mb-0">EVENTANO is a place where you can find all the events you need for you to enjoy or learn from.</p>
+                        <p>Website created by <a href="https://www.linkedin.com/in/mbazych/">Michał Bazych</a> and <a href="https://www.linkedin.com/in/mateusz-czarczy%C5%84ski-1263a5173/">Mateusz Czarczyński</a></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright © EVENTANO.com 2020</small></div>
+        </div>
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
+        <div class="scroll-to-top d-lg-none position-fixed">
+            <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src="./static/assets/mail/jqBootstrapValidation.js"></script>
+        <script src="./static/assets/mail/contact_me.js"></script>
+        <!-- Core theme JS-->
+        <script src="static/js/scripts.js"></script>
+    </body>
 </html>
---
+++ src/scripts/add_user.php
@@ -0,0 +1,86 @@
+<?
+    session_start();
+    if(!empty($_POST['name']) &&  !empty($_POST['surname']) && !empty($_POST['email'])&& !empty($_POST['city'])&& !empty($_POST['nationality']) && !empty($_POST['email2']) && !empty($_POST['pass']) && !empty($_POST['pass2']) && !empty($_POST['birthday'])){
+
+        if(!isset($_POST['terms'])){
+            $_SESSION['error'] = "Zaznacz pole z regulaminem";
+            ?>
+            <script>
+                window.history.back();
+            </script>
+            <?
+        
+        }
+
+        if($_POST['email']!=$_POST['email2']){
+            $_SESSION['error'] = "Emaile są rózne";
+            ?>
+            <script>
+                window.history.back();
+            </script>
+            <?
+        }
+
+        if($_POST['pass']!=$_POST['pass2']){
+            $_SESSION['error'] = "Hasła są rózne";
+            ?>
+            <script>
+                window.history.back();
+            </script>
+            <?
+        }
+
+        require_once './connect.php';
+
+        if($conn->connect_errno){
+            $_SESSION['error']="Błędne połączenie z bazą danych";
+            header("location: ../pages/register.php");
+        }else{
+            $city=$_POST['city'];
+            $nationality=$_POST['nationality'];
+            $name=$_POST['name'];
+            $surname=$_POST['surname'];
+            $email=$_POST['email'];
+            $pass=$_POST['pass'];
+            // szyfrowanko z ARGON2ID
+            // $pass = password_hash($pass2,PASSWORD_ARGON2ID); //argon nie dziala
+            $birthday=$_POST['birthday'];
+            $sql= "INSERT INTO `user`(`name`, `surname`, `city_id`, `nationality_id`, `email`, `pass`)
+            VALUES (?,?,?,?,?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ssiiss',$name,$surname,$city,$nationality,$email,$pass);
+            $i=$stmt->execute();
+            if($i){
+                header("location: ../index.php?register=success");
+                exit();
+            }else{
+
+                $zapytanie = "SELECT * FROM `user` WHERE email=?";
+                $statement = $conn->prepare($zapytanie);
+                $statement->bind_param('s',$email);
+                $statement->execute();
+                $wpisz = $statement->get_result();
+                $count=0;
+                while($rekord = $wpisz->fetch_assoc()){
+                    $count++;
+                }
+                if($count>0)
+                    $_SESSION['error']="Email juz istnieje w bazie";
+                else
+                    $_SESSION['error']= 'Cos poszlo nie tak';
+                header("location: ../pages/register.php");
+            }
+        
+        
+        }
+
+
+    }else{
+        $_SESSION['error'] = "Wypełnij wszystkie pola";
+        ?>
+            <script>
+                window.history.back();
+            </script>
+        <?
+    }
+?>

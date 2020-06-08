08dc5fb2523ffce1c8595f8a87b0e6bb960c78a7
Mateusz Czarczyński <mczarczynski@edu.cdv.pl>
2020-06-08 23:24:01 +0200

    asd

--- src/pages/new_event.php
+++ src/pages/new_event.php
@@ -50,6 +50,13 @@ if (!isset($_SESSION['logged']['email'])) {
       <a class="navbar-brand js-scroll-trigger" href="../index.php">EVENTANO</a><button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
+        <?
+                    if(isset($_SESSION['logged']['email'])){
+                    ?>
+                    <li class="nav-item mx-0 mx-lg-1"><a style="padding:0px !important;" class="nav-link py-3 px-0 px-lg-3 rounded" href="pages/profile_page.php"><i style="font-size:3.5rem;" class="fa fa-user-circle" aria-hidden="true"></i></a></li>
+                    <?
+                    }
+                    ?>
           <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../events.php">Events</a></li>
           <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../#categories">Categories</a></li>
           <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../#contact">Contact</a></li>
